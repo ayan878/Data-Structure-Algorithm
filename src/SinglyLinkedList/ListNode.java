92f275a72d884ee749b15fae52941f58fd33c64b
ayan878 <ayanraza197@gmail.com>
2024-06-09 16:17:30 +0530

    ListNode

---
+++ src/SinglyLinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package SinglyLinkedList;
+
+public class ListNode {
+    int data;
+    ListNode next;
+
+    public ListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
