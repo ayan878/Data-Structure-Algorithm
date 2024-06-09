b2d5d6a718d1cae5c2c1362a0f2f9c3d78f905ee
ayan878 <ayanraza197@gmail.com>
2024-06-09 16:16:25 +0530

    SinglyLinkedListStack

--- src/SinglyLinkedList/SinglyLinkedListStack.java
+++ src/SinglyLinkedList/SinglyLinkedListStack.java
@@ -2,21 +2,20 @@ package SinglyLinkedList;
 
 public class SinglyLinkedListStack {
     ListNode head;
-    public static class ListNode{
-        ListNode next;
-        int value;
 
-        public ListNode(int value){
-            this.next=null;
-            this.value=value;
-        }
+    public SinglyLinkedListStack(){
+        this.head=null;
     }
-
+    public void push(int data){
+        ListNode newNode=new ListNode(data);
+        newNode.next=head;
+        head=newNode;
+    }
     public void traverse() {
         ListNode current=head;
 
         while (current!=null){
-            System.out.print(current.value+"-->");
+            System.out.print(current.data+"-->");
             current=current.next;
         }
         System.out.println("null");
@@ -24,15 +23,20 @@ public class SinglyLinkedListStack {
     }
     public static void main(String[] args) {
         SinglyLinkedListStack sllStack=new SinglyLinkedListStack();
-        sllStack.head=new ListNode(11);
-        ListNode second=new ListNode(5);
-        ListNode third=new ListNode(7);
-        ListNode forth=new ListNode(3);
+//        sllStack.head=new ListNode(11);
+//        ListNode second=new ListNode(5);
+//        ListNode third=new ListNode(7);
+//        ListNode forth=new ListNode(3);
+//
+//        sllStack.head.next=second;
+//        second.next=third;
+//        third.next=forth;
+//        forth.next=null;
 
-        sllStack.head.next=second;
-        second.next=third;
-        third.next=forth;
-        forth.next=null;
+        sllStack.push(11);
+        sllStack.push(7);
+        sllStack.push(3);
+        sllStack.push(5);
 
         // Display List
         sllStack.traverse();
