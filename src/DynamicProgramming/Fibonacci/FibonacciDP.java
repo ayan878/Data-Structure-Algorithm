df44006b97c5f557960af19f5cf7363f435961dd
ayan878 <ayanraza197@gmail.com>
2024-11-11 01:50:55 +0530

    Dynamic Programming

---
+++ src/DynamicProgramming/Fibonacci/FibonacciDP.java
@@ -0,0 +1,16 @@
+package DynamicProgramming.Fibonacci;
+
+public class FibonacciDP {
+    public static void main(String[] args) {
+        int n=6;
+        int first=0;
+        int second=1;
+        int sumOfFib=0;
+        for (int i = 2; i <=n ; i++) {
+            sumOfFib=first+second;
+            first=second;
+            second=sumOfFib;
+        }
+        System.out.println(sumOfFib);
+    }
+}
